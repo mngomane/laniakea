d88e32cef89086d8a3d0b3cfbfc0c82c6ca18a6f
mngomane <max.ngomane@gmail.com>
2019-05-03 20:44:35 +0200

    makefile update && install java option added

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,9 @@ if [ ! -d /usr/local/go ];
 then
   curl https://dl.google.com/go/go1.12.4.darwin-amd64.pkg -o go1.12.4.darwin-amd64.pkg;
   sudo installer -pkg go1.12.4.darwin-amd64.pkg -target /;
-  export PATH=$PATH:/usr/local/go/bin;
+  echo "export GOROOT=/usr/local/go" >> ~/.bash_profile;
+  echo "export GOPATH=$HOME/go" >> ~/.bash_profile;
+  echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> ~/.bash_profile;
+  echo "export PATH=$PATH:/usr/local/go/bin;" >> ~/.bash_profile;
   rm go1.12.4.darwin-amd64.pkg;
 fi
